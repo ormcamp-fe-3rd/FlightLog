ee9f3e30cc4ba3c8c68b2d9ea1f9c44f7693ba8f
mycreature <aistjq1691@gmail.com>
2025-01-08 14:46:52 +0900

    [chore] tailwind 기본 설정 config 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,16 @@ export default {
       fontFamily: {
         sans: ["var(--font-pretendard)"],
       },
+      fontSize: {
+        'xl'  : '1.125rem', // 18px
+        '2xl' : '1.875rem', // 30px
+      },
+      screens: {
+        lg: { max: '1024px'},
+        md: { max: '744px'},
+        sm: { max: '375px'}
+      }
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 } satisfies Config;
