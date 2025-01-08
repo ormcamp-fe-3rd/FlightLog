172afa4294035f5a1dd470196485b6bf0831b64b
oratio100 <oratio100@gmail.com>
2025-01-08 17:25:41 +0900

    [design] StatusPanel 레이아웃

--- src/components/map/StatusPanel.tsx
+++ src/components/map/StatusPanel.tsx
@@ -1,7 +1,70 @@
 export default function StatusPanel() {
   return (
-    <>
-      <div>드론 자세 패널</div>
-    </>
+    <div className="flex max-h-full scroll-m-0 flex-col gap-6 overflow-y-scroll py-6 pl-10">
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-4">
+          <img src="/images/map/icon-GPS.svg"></img>
+          <div>GPS 정보</div>
+        </div>
+        <div className="flex">
+          <div className="flex-1">
+            <div>위도</div>
+            <div>위도</div>
+          </div>
+          <div className="flex-1">
+            <div>경도</div>
+            <div>경도</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-4">
+          <img src="/images/map/icon-Flight.svg"></img>
+          <div>비행 정보</div>
+        </div>
+        <div className="flex">
+          <div className="flex-1">
+            <div>속도</div>
+            <div>속도</div>
+          </div>
+          <div className="flex-1">
+            <div>방향</div>
+            <div>방향</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-4">
+          <img src="/images/map/icon-Altitude.svg"></img>
+          <div>고도 정보</div>
+        </div>
+        <div className="flex">
+          <div className="flex-1">
+            <div>상대 고도</div>
+            <div>상대 고도</div>
+          </div>
+          <div className="flex-1">
+            <div>절대 고도</div>
+            <div>절대 고도</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-4">
+          <img src="/images/map/icon-Accuracy.svg"></img>
+          <div>정확도</div>
+        </div>
+        <div className="flex">
+          <div className="flex-1">
+            <div>수평 정확도</div>
+            <div>수평 정확도</div>
+          </div>
+          <div className="flex-1">
+            <div>수직 정확도</div>
+            <div>수직 정확도</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
