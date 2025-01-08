fcf2b27d16971dc35d8e620c3c95b3b7f8541db2
oratio100 <oratio100@gmail.com>
2025-01-08 16:09:49 +0900

    [feat] map 지도 불러오기

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -8,15 +8,17 @@ export default function MapPage() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="relative flex-1">
-        <MapView />
-        <div className="absolute right-8 top-8 w-[280px] rounded-[30px] bg-gray-500">
+      <div className="relative h-[calc(100vh-56px)] flex-1">
+        <div className="h-full">
+          <MapView />
+        </div>
+        <div className="absolute right-8 top-8 z-10 w-[280px] rounded-[30px] bg-gray-500">
           <StatusPanel />
         </div>
-        <div className="absolute bottom-20 right-8 size-[280px] rounded-[30px] bg-gray-400">
+        <div className="absolute bottom-20 right-8 z-10 size-[280px] rounded-[30px] bg-gray-400">
           <AttitudePanel />
         </div>
-        <div className="absolute bottom-7 left-1/2 w-1/3 -translate-x-1/2">
+        <div className="absolute bottom-7 left-1/2 z-10 w-1/3 -translate-x-1/2">
           <FlightProgressBar />
         </div>
       </div>
