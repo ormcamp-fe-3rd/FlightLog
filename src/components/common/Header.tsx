3aa0d89f40ea2c28e96c5c2decebd07027102dad
oratio100 <oratio100@gmail.com>
2025-01-09 12:32:24 +0900

    [design] 헤더 반응형 레이아웃

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex h-14 items-center justify-between bg-black p-4 text-white">
-      <Link href="/" className="cursor-pointer">
-        FlightLog
-      </Link>
+      <div className="flex items-center gap-5">
+        <button className="hidden size-5 md:block">
+          <img src="/images/map/icon-menu.svg" alt="Toggle sidebar" />
+        </button>
+        <Link href="/" className="cursor-pointer text-2xl font-bold">
+          FlightLog
+        </Link>
+      </div>
       <Link href="/login">Login</Link>
     </header>
   );
