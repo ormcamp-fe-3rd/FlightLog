c298fe07ea546bad40fc87453e5bf471028858e9
oratio100 <oratio100@gmail.com>
2025-01-10 16:43:21 +0900

    [remove] 폴더 구조 변경(utils)

---
+++ src/utils/database.ts
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+declare global {
+  var _mongo: Promise<MongoClient> | undefined;
+}
+
+const url = process.env.MONGODB_URL;
+if (!url) throw new Error("DB URL 오류입니다.");
+
+let connectDB: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url).connect();
+  }
+  connectDB = global._mongo;
+} else {
+  connectDB = new MongoClient(url).connect();
+}
+export { connectDB };
