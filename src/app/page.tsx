dff8b8098178549a5949550fc4c9a9c56b320cd8
incolore9 <hansol.woff@gmail.com>
2025-01-10 22:15:10 +0900

    [design] 로그인 모달창 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import LoginModal from "@/components/home/LoginModal";
 import ServiceStart from "@/components/home/ServiceStart";
 import ImageGallery from "@/components/home/ImageGallery";
 import ServiceTitle from "@/components/home/ServiceTitle";
@@ -6,12 +7,15 @@ import DataService from "@/components/home/DataService";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-7xl break-keep px-4 py-12">
-      <ServiceStart />
-      <ImageGallery />
-      <ServiceTitle />
-      <Cards />
-      <DataService />
-    </div>
+    <>
+      <LoginModal />
+      <div className="mx-auto max-w-7xl break-keep px-4 py-12">
+        <ServiceStart />
+        <ImageGallery />
+        <ServiceTitle />
+        <Cards />
+        <DataService />
+      </div>
+    </>
   );
 }
