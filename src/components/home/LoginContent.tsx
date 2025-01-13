83ed98c8b4b68e88cc51fa3e4e38b0f733832c3d
incolore9 <hansol.woff@gmail.com>
2025-01-13 13:52:26 +0900

    [feat] submit 버튼 비활성화 / 활성화 추가

--- src/components/home/LoginContent.tsx
+++ src/components/home/LoginContent.tsx
@@ -1,7 +1,20 @@
 import useLoginModalStore from "@/store/useLoginModal";
+import { useForm } from "react-hook-form";
 
 export default function LoginContent() {
   const { toggle } = useLoginModalStore();
+
+  const {
+    register,
+    formState: { isValid },
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <>
       <div className="flex justify-between">
@@ -10,11 +23,13 @@ export default function LoginContent() {
           ✖️
         </button>
       </div>
+
       <form className="text-black" noValidate>
         <label htmlFor="email" className="label label-text pb-0">
           이메일
         </label>
         <input
+          {...register("email", { required: true })}
           type="email"
           id="email"
           className="input input-sm input-bordered mt-1 w-full focus:border-blue-500 focus:outline-none"
@@ -25,13 +40,18 @@ export default function LoginContent() {
           비밀번호
         </label>
         <input
+          {...register("password", { required: true })}
           type="password"
           id="password"
           className="input input-sm input-bordered mt-1 w-full focus:border-blue-500 focus:outline-none"
           required
         />
 
-        <button type="submit" className="btn btn-primary mt-8 w-full">
+        <button
+          type="submit"
+          className={`btn mt-8 w-full ${isValid ? "btn-primary" : "btn-disabled"}`}
+          disabled={!isValid}
+        >
           로그인
         </button>
       </form>
