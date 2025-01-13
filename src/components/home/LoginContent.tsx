eb8e966c4b782d49195f1a40fbe9127fdf0ece90
incolore9 <hansol.woff@gmail.com>
2025-01-13 12:45:55 +0900

    [design] 인풋 간격 조정, 한국어 ui로 변경 , span 정리, placeholder 추가

--- src/components/home/LoginContent.tsx
+++ src/components/home/LoginContent.tsx
@@ -5,35 +5,34 @@ export default function LoginContent() {
   return (
     <>
       <div className="flex justify-between">
-        <h2 className="mb-2 text-2xl font-bold text-black">Login</h2>
+        <h2 className="mb-4 text-2xl font-bold text-black">로그인</h2>
         <button className="btn btn-circle btn-ghost" onClick={toggle}>
           ✖️
         </button>
       </div>
-      <form className="text-black">
-        <label htmlFor="email" className="label">
-          <span className="label-text">e-mail</span>
+      <form className="text-black" noValidate>
+        <label htmlFor="email" className="label label-text pb-0">
+          이메일
         </label>
         <input
           type="email"
           id="email"
-          className="input input-sm input-bordered w-full focus:border-blue-500 focus:outline-none"
+          className="input input-sm input-bordered mt-1 w-full focus:border-blue-500 focus:outline-none"
           required
         />
 
-        <div className="mb-8">
-          <label htmlFor="password" className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="input input-sm input-bordered w-full"
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary w-full">
-          Login
+        <label htmlFor="password" className="label label-text mt-2 pb-0">
+          비밀번호
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="input input-sm input-bordered mt-1 w-full focus:border-blue-500 focus:outline-none"
+          required
+        />
+
+        <button type="submit" className="btn btn-primary mt-8 w-full">
+          로그인
         </button>
       </form>
     </>
--- src/components/home/RegisterContent.tsx
+++ src/components/home/RegisterContent.tsx
@@ -10,7 +10,15 @@ export default function RegisterContent() {
   const schema = z
     .object({
       name: z.string().min(2, { message: "이름은 2글자 이상이어야 합니다." }),
-      email: z.string().email({ message: "올바른 이메일을 입력해주세요." }),
+      email: z
+        .string()
+        .email({ message: "올바른 이메일을 입력해주세요." })
+        .refine(
+          (val) => /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(val),
+          {
+            message: "올바른 이메일 형식이 아닙니다.",
+          },
+        ),
       password: z
         .string()
         .min(5, { message: "비밀번호는 최소 5자 이상이어야 합니다." })
@@ -20,15 +28,7 @@ export default function RegisterContent() {
         .refine((val) => /[!@#$%^&*]/.test(val), {
           message: "특수문자를 포함해야 합니다.",
         }),
-      passwordConfirm: z
-        .string()
-        .min(5, { message: "비밀번호는 최소 5자 이상이어야 합니다." })
-        .refine((val) => /[0-9]/.test(val), {
-          message: "숫자를 포함해야 합니다.",
-        })
-        .refine((val) => /[!@#$%^&*]/.test(val), {
-          message: "특수문자를 포함해야 합니다.",
-        }),
+      passwordConfirm: z.string(),
     })
     .refine((data) => data.password === data.passwordConfirm, {
       path: ["passwordConfirm"],
@@ -51,22 +51,28 @@ export default function RegisterContent() {
     },
   });
 
+  const onSubmit = (data: any) => {
+    //추후에 회원가입 로직 추가하기
+    console.log(data);
+  };
+
   return (
-    <>
+    <div>
       <div className="flex justify-between">
-        <h2 className="mb-4 text-2xl font-bold text-black">Register</h2>
+        <h2 className="mb-4 text-2xl font-bold text-black">회원 가입</h2>
         <button className="btn btn-circle btn-ghost" onClick={toggle}>
           ✖️
         </button>
       </div>
-      <form onSubmit={handleSubmit(onsubmit)} className="text-black" noValidate>
-        <label htmlFor="name" className="label">
-          <span className="label-text">name</span>
+      <form onSubmit={handleSubmit(onSubmit)} className="text-black" noValidate>
+        <label htmlFor="name" className="label label-text pb-0">
+          이름
         </label>
         <input
           type="text"
           id="name"
-          className={`input input-sm input-bordered w-full ${errors.name ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
+          placeholder="2글자 이상 입력해주세요"
+          className={`input input-sm input-bordered mt-1 w-full ${errors.name ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
           required
           {...register("name")}
         />
@@ -74,13 +80,14 @@ export default function RegisterContent() {
           <p className="mt-1 text-xs text-error">{errors.name.message}</p>
         )}
 
-        <label htmlFor="email" className="label">
-          <span className="label-text">e-mail</span>
+        <label htmlFor="email" className="label label-text mt-2 pb-0">
+          이메일
         </label>
         <input
           type="email"
           id="email"
-          className={`input input-sm input-bordered w-full ${errors.email ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
+          placeholder="이메일 형식으로 입력해주세요"
+          className={`input input-sm input-bordered mt-1 w-full ${errors.email ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
           required
           {...register("email")}
         />
@@ -88,13 +95,14 @@ export default function RegisterContent() {
           <p className="mt-1 text-xs text-error">{errors.email.message}</p>
         )}
 
-        <label htmlFor="password" className="label">
-          <span className="label-text">password</span>
+        <label htmlFor="password" className="label label-text mt-2 pb-0">
+          비밀번호
         </label>
         <input
           type="password"
           id="password"
-          className={`input input-sm input-bordered w-full ${errors.password ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
+          placeholder="숫자, 특수문자를 포함한 5자 이상 입력해주세요."
+          className={`input input-sm input-bordered mt-1 w-full ${errors.password ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
           required
           {...register("password")}
         />
@@ -102,13 +110,14 @@ export default function RegisterContent() {
           <p className="mt-1 text-xs text-error">{errors.password.message}</p>
         )}
 
-        <label htmlFor="passwordConfirm" className="label">
-          <span className="label-text">password confirm</span>
+        <label htmlFor="passwordConfirm" className="label label-text mt-2 pb-0">
+          비밀번호 확인
         </label>
         <input
           type="password"
           id="passwordConfirm"
-          className={`input input-sm input-bordered w-full ${errors.passwordConfirm ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
+          placeholder="위와 동일한 비밀번호를 입력해주세요"
+          className={`input input-sm input-bordered mt-1 w-full ${errors.passwordConfirm ? "border-red-500 focus:border-red-500" : "focus:border-blue-500"} focus:outline-none`}
           required
           {...register("passwordConfirm")}
         />
@@ -118,10 +127,10 @@ export default function RegisterContent() {
           </p>
         )}
 
-        <button type="submit" className="btn btn-primary mt-6 w-full">
+        <button type="submit" className="btn btn-primary mt-8 w-full">
           Register
         </button>
       </form>
-    </>
+    </div>
   );
 }
