88dc042d8232f4b4fa9169b48a9a9bc546b8fa91
oratio100 <oratio100@gmail.com>
2025-01-14 21:49:16 +0900

    [feedback] 타임스탬프 변환 함수(formatTimestamp) JSDoc 추가

--- src/utils/formatTimestamp.ts
+++ src/utils/formatTimestamp.ts
@@ -1,7 +1,13 @@
+/**
+ * ISO 8601 형식의 타임스탬프 문자열을 한국시각으로 변환
+ *
+ * @param {string} timestamp - ISO 8601 형식의 타임스탬프 문자열
+ * @returns {string} "YYYY. MM. DD. HH:mm" 형식의 한국 시각 (유효하지 않은 타임스탬프가 전달되면 "Invalid Date" 반환)
+ */
+
 export function formatTimestamp(timestamp: string) {
   const date = new Date(timestamp);
 
-  // 원하는 형식으로 날짜와 시간을 포맷
   const formattedDate = date
     .toLocaleString("ko-KR", {
       year: "numeric",
