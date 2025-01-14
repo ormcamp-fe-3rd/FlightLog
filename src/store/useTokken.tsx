7d278f3a3ed2ceb2b7a7aa1a6a1e1bef857cb798
incolore9 <hansol.woff@gmail.com>
2025-01-14 17:00:58 +0900

    [feat] 구글 로그인 추가

--- src/store/useTokken.tsx
+++
@@ -1,7 +0,0 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-
-export default async function Page() {
-  let session = await getServerSession(authOptions);
-  return <>{console.log(session)}</>;
-}
