a97c0eb004408f0bb710846a0af799a92e4345c3
incolore9 <hansol.woff@gmail.com>
2025-01-14 16:28:03 +0900

    [desgin] 로그인하면 버튼이 logout으로 변경 (useSession)

--- src/components/common/LoginButton.tsx
+++ src/components/common/LoginButton.tsx
@@ -2,11 +2,16 @@
 import useLoginModalStore from "@/store/useLoginModal";
 import AuthModal from "@/components/home/AuthModal";
 
+import { useSession } from "next-auth/react";
+
 export default function LoginButton() {
+  let session = useSession();
   const { toggle } = useLoginModalStore();
   return (
     <>
-      <button onClick={toggle}>Login</button>
+      <button onClick={toggle}>
+        {session.status === "authenticated" ? "logout" : "login"}
+      </button>
       <AuthModal />
     </>
   );
