3ee7f048e780d1583499f96727589a2f94e01e15
incolore9 <hansol.woff@gmail.com>
2025-01-14 17:00:58 +0900

    [feat] 구글 로그인 추가

--- src/components/home/SocialLoginButton.tsx
+++ src/components/home/SocialLoginButton.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import { signIn, signOut } from "next-auth/react";
+import { signIn } from "next-auth/react";
 
 export default function SocialLoginButton() {
   return (
-    <button
-      className="btn btn-primary btn-sm mt-4 w-full rounded-full"
-      onClick={() => signIn()}
-    >
-      GitHub로 로그인
-    </button>
+    <>
+      <button
+        className="btn btn-primary btn-sm mt-4 w-full rounded-full"
+        onClick={() => signIn("github")}
+      >
+        GitHub로 로그인
+      </button>
+
+      <button
+        className="btn btn-secondary btn-sm mt-4 w-full rounded-full"
+        onClick={() => signIn("google")}
+      >
+        Google로 로그인
+      </button>
+    </>
   );
 }
