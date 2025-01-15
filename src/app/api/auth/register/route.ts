62a60c3d9dd06c7d8e62b9692a61ff7207881b14
incolore9 <hansol.woff@gmail.com>
2025-01-15 18:08:59 +0900

    [feat] 회원가입 입력폼 DB로 전송만 1차 러프 구현

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "../../../../../util/database";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const data = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    const hash = await bcrypt.hash(data.password as string, 10);
+    data.password = hash;
+
+    const db = (await connectDB).db("user");
+    await db.collection("account").insertOne(data);
+
+    return NextResponse.redirect(new URL("/", req.url));
+  } catch (error) {
+    return NextResponse.json({ message: "가입 실패" }, { status: 500 });
+  }
+}
