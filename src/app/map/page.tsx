8e69847b56da62a6bfe8e66ec4ae03b3e98ee4e1
oratio100 <oratio100@gmail.com>
2025-01-17 01:02:31 +0900

    [feat] 비행 선택 드롭박스 추가

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -6,15 +6,21 @@ import AttitudePanel from "@/components/map/AttitudePanel";
 import FlightProgressBar from "@/components/map/FlightProgressBar";
 import MapView from "@/components/map/MapView";
 import ControlPanel from "@/components/map/ControlPanel";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useSidebarStore from "@/store/useSidebar";
 import useResizePanelControl from "@/hooks/useResizePanelControl";
+import SelectFlightLog from "@/components/map/SelectFlightLog";
 
 export default function MapPage() {
   const { isSidebarOpen } = useSidebarStore();
   const { isStatusOpen, setIsStatusOpen, isAttitudeOpen, setIsAttitudeOpen } =
     useResizePanelControl();
+  const [selectedFlight, setSelectedFlight] = useState("");
   const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setProgress(0);
+  }, [selectedFlight]);
 
   const toggleStatusPanel = () => {
     setIsStatusOpen(!isStatusOpen);
@@ -37,9 +43,10 @@ export default function MapPage() {
       </div>
       <div className="relative h-full min-w-[344px] flex-1 border-red-600">
         <div className="h-full">
-          <MapView progress={progress} />
+          <MapView selectedFlight={selectedFlight} progress={progress} />
         </div>
         <div className="absolute right-8 top-8 z-10 flex h-[90%] flex-col gap-4">
+          <SelectFlightLog onSelect={setSelectedFlight} />
           <div
             className={`${isStatusOpen ? "block" : "hidden"} overflow-hidden`}
           >
@@ -50,7 +57,9 @@ export default function MapPage() {
           </div>
         </div>
         <div className="absolute bottom-7 left-1/2 z-10 w-1/2 min-w-80 -translate-x-1/2">
-          <FlightProgressBar progress={progress} setProgress={setProgress} />
+          {selectedFlight !== "" && (
+            <FlightProgressBar progress={progress} setProgress={setProgress} />
+          )}
           <div className="flex justify-center">
             <ControlPanel
               onFlightInfoClick={toggleStatusPanel}
