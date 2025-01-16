9ed6a920929b5f41671bfd9fca63b33ed7c0bd4a
oratio100 <oratio100@gmail.com>
2025-01-17 03:49:58 +0900

    [feat] 마커 클릭 시 비행 선택 연동

--- src/components/map/SelectFlightLog.tsx
+++ src/components/map/SelectFlightLog.tsx
@@ -1,16 +1,27 @@
 import useData from "@/store/useData";
 
 interface SelectFlightLogProps {
+  value: string;
   onSelect: (value: string) => void;
+  setProgress: (value: number) => void;
 }
 
-export default function SelectFlightLog({ onSelect }: SelectFlightLogProps) {
+export default function SelectFlightLog({
+  value,
+  onSelect,
+  setProgress,
+}: SelectFlightLogProps) {
   const { selectedOperationId, validOperationLabels } = useData();
+  const handleSelection = (value: string) => {
+    onSelect(value);
+    setProgress(0);
+  };
   return (
     <div className="w-[280px]">
       <select
         className="select select-bordered w-full max-w-xs"
-        onChange={(e) => onSelect(e.target.value)}
+        onChange={(e) => handleSelection(e.target.value)}
+        value={value}
       >
         <option value="all">Select Operation</option>
         {selectedOperationId.map((id) => {
