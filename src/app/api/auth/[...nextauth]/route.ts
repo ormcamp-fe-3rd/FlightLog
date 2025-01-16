7104f8c19424c9f4daa742b4c1468aa3936b5a72
incolore9 <hansol.woff@gmail.com>
2025-01-16 11:53:04 +0900

    [refactor] 피드백 반영: env 누락시, 런타임 단계에서 에러로 인식하도록 수정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,15 +2,25 @@ import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
+function getRequiredEnvVar(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(
+      `${name} environment variable is required but not provided.`,
+    );
+  }
+  return value;
+}
+
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      clientId: getRequiredEnvVar("GITHUB_CLIENT_ID"),
+      clientSecret: getRequiredEnvVar("GITHUB_CLIENT_SECRET"),
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: getRequiredEnvVar("GOOGLE_CLIENT_ID"),
+      clientSecret: getRequiredEnvVar("GOOGLE_CLIENT_SECRET"),
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
