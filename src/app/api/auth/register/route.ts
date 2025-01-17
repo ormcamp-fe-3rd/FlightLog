9598d38929bf743c4b556b0796504da200b6773e
incolore9 <hansol.woff@gmail.com>
2025-01-17 19:47:16 +0900

    [rename] 불필요 폴더 삭제, 드론 마커 수정, 데이지 light 테마 지정

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/database";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
   try {
