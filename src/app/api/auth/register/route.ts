ced8bcd95f51ba746b199d0c9fea28041a587186
incolore9 <hansol.woff@gmail.com>
2025-01-17 14:57:04 +0900

    [rename] 폴더명, 파일 위치를 map브랜치 기준으로 일부 변경

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "../../../../util/common/database";
+import { connectDB } from "@/lib/database";
 import bcrypt from "bcrypt";
 
 export async function POST(req: Request) {
