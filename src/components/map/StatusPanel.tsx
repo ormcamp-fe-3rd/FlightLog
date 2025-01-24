97bdfb36f609a70ec915687985f4e48a2063c987
oratio100 <oratio100@gmail.com>
2025-01-24 15:20:21 +0900

    [modify] StatusPanel 비행시간 수정 (경과시간)

--- src/components/map/StatusPanel.tsx
+++ src/components/map/StatusPanel.tsx
@@ -50,7 +50,7 @@ export default function StatusPanel({
       <div className="flex flex-col gap-2">
         <div className="flex gap-4">
           <img src="/images/map/icon-Accuracy.svg" alt="Accuracy" />
-          <div>속도 정보</div>
+          <div>속도/시간 정보</div>
         </div>
         <div className="flex">
           <div className="flex-1">
@@ -58,7 +58,7 @@ export default function StatusPanel({
             <div>{statusData?.groundSpeed}</div>
           </div>
           <div className="flex-1">
-            <div>총 비행 시간</div>
+            <div>비행 시간</div>
             <div>{statusData?.flightTime}</div>
           </div>
         </div>
