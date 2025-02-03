c54b35087fd8340c2801297a09b61a586d492759
incolore9 <hansol.woff@gmail.com>
2025-02-03 17:45:51 +0900

    [feat] 렌더링 최적화 추가

--- src/components/log/charts/MultipleAxesCharts.tsx
+++ src/components/log/charts/MultipleAxesCharts.tsx
@@ -175,6 +175,7 @@ const BatteryStatusChart = () => {
             enabled: true,
           },
           showInNavigator: true,
+          turboThreshold: 300, //데이터 포인트를 1000개 이상 처리할 때 렌더링 최적화
         },
       },
       rangeSelector: {
