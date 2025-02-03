f72cf75edd341a8c1568c8186f2eeb7ef7d29ef1
incolore9 <hansol.woff@gmail.com>
2025-02-04 01:44:33 +0900

    [rename] 폴더 구조 변경 후 지연님 코드로 대체
    Co-authored-by: jiyeon <oratio100@gmail.com>

--- src/app/(data)/map/page.tsx
+++ src/app/(data)/map/page.tsx
@@ -1,24 +1,54 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import Sidebar from "@/components/common/Sidebar";
 import StatusPanel from "@/components/map/StatusPanel";
 import AttitudePanel from "@/components/map/AttitudePanel";
 import FlightProgressBar from "@/components/map/FlightProgressBar";
-import ControlPanel from "@/components/map/ControlPanel";
-import { useState } from "react";
-import useResizePanelControl from "@/hooks/useResizePanelControl";
 import SelectFlightLog from "@/components/map/SelectFlightLog";
-
-const MapView = dynamic(() => import("@/components/map/MapView"), {
-  ssr: false,
-  loading: () => <div>Loading map...</div>,
-}); // 500오류 수정:  "window is not defined" SSR
+import MapView from "@/components/map/MapView";
+import ControlPanel from "@/components/map/ControlPanel";
+import { useEffect, useState } from "react";
+import useData from "@/store/useData";
+import useSidebarStore from "@/store/useSidebar";
+import useResizePanelControl from "@/hooks/useResizePanelControl";
+import useOperationData from "@/hooks/useOperationData";
+import { INITIAL_POSITION } from "@/constants";
 
 export default function MapPage() {
+  const { isSidebarOpen } = useSidebarStore();
   const { isStatusOpen, setIsStatusOpen, isAttitudeOpen, setIsAttitudeOpen } =
     useResizePanelControl();
-  const [selectedFlight, setSelectedFlight] = useState("all");
+  const { selectedOperationId } = useData();
+  const { updatedTimestamps, updatedStartPoint } = useOperationData();
+  const [selectedFlight, setSelectedFlight] = useState(selectedOperationId[0]);
   const [progress, setProgress] = useState(0);
+  const [operationTimestamps, setOperationTimestamps] = useState<
+    Record<string, number[]>
+  >({});
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [allTimestamps, setAllTimestamps] = useState<number[]>([]);
+  const [dronePositions, setDronePositions] = useState<
+    { flightId: string; position: [number, number]; direction: number }[]
+  >([]);
+  const [mapPosition, setMapPosition] = useState<[number, number]>([
+    INITIAL_POSITION.LAT,
+    INITIAL_POSITION.LNG,
+  ]);
+
+  useEffect(() => {
+    const sortedAllTimestamps = Object.values(updatedTimestamps)
+      .flat()
+      .sort((a, b) => a - b);
+    setOperationTimestamps(updatedTimestamps);
+    setAllTimestamps(sortedAllTimestamps);
+
+    const initialPosition = updatedStartPoint[selectedOperationId[0]];
+    setMapPosition(initialPosition);
+  }, [updatedTimestamps, updatedStartPoint]);
+
+  useEffect(() => {
+    setSelectedFlight(selectedOperationId[0]);
+  }, [selectedOperationId]);
 
   const toggleStatusPanel = () => {
     setIsStatusOpen(!isStatusOpen);
@@ -29,39 +59,79 @@ export default function MapPage() {
   };
 
   const zoomToDrone = () => {
-    // Todo
+    const selectedDronePosition = dronePositions.find(
+      (drone) => drone.flightId === selectedFlight,
+    )?.position;
+
+    if (selectedDronePosition) {
+      setMapPosition(selectedDronePosition);
+    } else {
+      console.warn(`Start point not found for ID: ${selectedFlight}`);
+    }
   };
 
   return (
-    <div className="relative h-full min-w-[344px] flex-1 border-red-600">
-      <div className="h-full">
-        <MapView
-          selectedFlight={selectedFlight}
-          progress={progress}
-          onMarkerClick={setSelectedFlight}
-        />
+    <div className="flex h-[calc(100vh-56px)] overflow-hidden">
+      <div
+        className={`${isSidebarOpen ? "md:block" : "md:hidden"} z-20 md:absolute`}
+      >
+        <Sidebar />
       </div>
-      <div className="absolute right-8 top-8 z-10 flex h-[90%] flex-col gap-4">
-        <SelectFlightLog
-          value={selectedFlight}
-          onSelect={setSelectedFlight}
-          setProgress={setProgress}
-        />
-        <div className={`${isStatusOpen ? "block" : "hidden"} overflow-hidden`}>
-          <StatusPanel />
-        </div>
-        <div className={`${isAttitudeOpen ? "block" : "hidden"}`}>
-          <AttitudePanel />
-        </div>
-      </div>
-      <div className="absolute bottom-7 left-1/2 z-10 w-1/2 min-w-80 -translate-x-1/2">
-        <FlightProgressBar progress={progress} setProgress={setProgress} />
-        <div className="flex justify-center">
-          <ControlPanel
-            onFlightInfoClick={toggleStatusPanel}
-            onAttitudeClick={toggleAttitudePanel}
-            onZoomClick={zoomToDrone}
+      <div className="relative h-full min-w-[344px] flex-1 border-red-600">
+        <div className="h-full">
+          <MapView
+            progress={progress}
+            operationTimestamps={operationTimestamps}
+            allTimestamps={allTimestamps}
+            onMarkerClick={setSelectedFlight}
+            mapPosition={mapPosition}
+            dronePositions={dronePositions}
+            setDronePositions={setDronePositions}
           />
+        </div>
+        <div className="absolute right-8 top-8 z-10 flex max-h-[90%] flex-col gap-4">
+          <SelectFlightLog
+            value={selectedFlight}
+            onSelect={setSelectedFlight}
+          />
+          <div
+            className={`${isStatusOpen ? "block" : "hidden"} overflow-hidden overflow-y-auto rounded-[30px]`}
+          >
+            <StatusPanel
+              progress={progress}
+              allTimestamps={allTimestamps}
+              operationTimestamps={operationTimestamps}
+              selectedOperationId={selectedOperationId}
+              selectedFlight={selectedFlight}
+            />
+          </div>
+          <div className={`${isAttitudeOpen ? "block" : "hidden"}`}>
+            <AttitudePanel
+              progress={progress}
+              allTimestamps={allTimestamps}
+              operationTimestamps={operationTimestamps}
+              selectedOperationId={selectedOperationId}
+              selectedFlight={selectedFlight}
+              isPlaying={isPlaying}
+            />
+          </div>
+        </div>
+        <div className="absolute bottom-7 left-1/2 z-10 flex w-2/3 min-w-80 -translate-x-1/2 flex-col gap-1">
+          <FlightProgressBar
+            progress={progress}
+            setProgress={setProgress}
+            allTimestamps={allTimestamps}
+            operationTimestamps={operationTimestamps}
+            setSelectedFlight={setSelectedFlight}
+            setIsPlaying={setIsPlaying}
+          />
+          <div className="flex justify-center">
+            <ControlPanel
+              onFlightInfoClick={toggleStatusPanel}
+              onAttitudeClick={toggleAttitudePanel}
+              onZoomClick={zoomToDrone}
+            />
+          </div>
         </div>
       </div>
     </div>
