742b1bde22060bc122454d00de244867729d6866
incolore9 <hansol.woff@gmail.com>
2025-02-04 13:48:25 +0900

    [fix] 비로그인 상태에서 다른 경로의 페이지 내용을 볼 수 있는 현상 수정

--- src/app/(data)/layout.tsx
+++ src/app/(data)/layout.tsx
@@ -2,11 +2,6 @@
 
 import Sidebar from "@/components/common/Sidebar";
 import useSidebarStore from "@/store/useSidebar";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import useLoginModalStore from "@/store/useLoginModal";
-import { AUTH_LOGIN } from "@/utils/AuthLogin";
 
 export default function DataLayout({
   children,
@@ -14,16 +9,6 @@ export default function DataLayout({
   children: React.ReactNode;
 }) {
   const { isSidebarOpen } = useSidebarStore();
-  const session = useSession();
-  const router = useRouter();
-  const { toggle } = useLoginModalStore();
-
-  useEffect(() => {
-    if (session.status === AUTH_LOGIN.STATUS.UNAUTHENTICATED) {
-      router.push("/");
-      alert("로그인한 유저만 이용할 수 있는 기능입니다.");
-    }
-  }, [session.status, toggle, router]);
 
   return (
     <div className="flex h-[calc(100vh-56px)] overflow-hidden">
