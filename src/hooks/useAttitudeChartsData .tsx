477dfc8b46992640418f31b761a5a9dacb4882a6
mycreature <aistjq1691@gmail.com>
2025-02-06 01:30:40 +0900

    [fix]차트 라벨링 변경 및 년도 제거

--- src/hooks/useAttitudeChartsData .tsx
+++ src/hooks/useAttitudeChartsData .tsx
@@ -112,7 +112,7 @@ const createChartOptions = (
         formatter: function (): any {
           const value = (this as any).value;
           const dateObj = new Date(value);
-          const formattedDate = `${String(dateObj.getMonth() + 1).padStart(2, "0")}-${String(dateObj.getDate()).padStart(2, "0")} ${String(dateObj.getHours()).padStart(2, "0")}:${String(dateObj.getMinutes()).padStart(2, "0")}:${String(dateObj.getSeconds()).padStart(2, "0")}`;
+          const formattedDate = `${String(dateObj.getHours()).padStart(2, "0")}:${String(dateObj.getMinutes()).padStart(2, "0")}:${String(dateObj.getSeconds()).padStart(2, "0")}`;
           return formattedDate;
         },
       },
@@ -141,7 +141,7 @@ const createChartOptions = (
     },
     responsive: {},
     series: groupDataKeys.map((key, idx) => ({
-      name: `${dataset.name} ${idx + 1}`,
+      name: new Date(groupData[key][0][0]).toLocaleString(),
       type: dataset.type,
       step: true,
       data: groupData[key],
--- src/hooks/useStatusChartsData.tsx
+++ src/hooks/useStatusChartsData.tsx
@@ -115,7 +115,7 @@ const createChartOptions = (
         formatter: function (): any {
           const value = (this as any).value;
           const dateObj = new Date(value);
-          const formattedDate = `${String(dateObj.getMonth() + 1).padStart(2, "0")}-${String(dateObj.getDate()).padStart(2, "0")} ${String(dateObj.getHours()).padStart(2, "0")}:${String(dateObj.getMinutes()).padStart(2, "0")}:${String(dateObj.getSeconds()).padStart(2, "0")}`;
+          const formattedDate = `${String(dateObj.getHours()).padStart(2, "0")}:${String(dateObj.getMinutes()).padStart(2, "0")}:${String(dateObj.getSeconds()).padStart(2, "0")}`;
           return formattedDate;
         },
       },
